4007578ee93f954e124dd780868067d982dc201e
Benjamin Good <benjamingood@google.com>
2025-07-09 18:22:28 +0000

    .... and tf format.

--- reference-architectures/backstage/backstage-quickstart/iap.tf
+++ reference-architectures/backstage/backstage-quickstart/iap.tf
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-data "google_project" "backstageProject" { 
+data "google_project" "backstageProject" {
   project_id = var.environment_project_id
 }
 
